e1e9261924b77c5a5f0b373256defd4536d23492
Weike Shi <shiw14@mcmaster.ca>
2020-11-05 18:57:02 -0500

    Q3, plot missing, config arrival rate

--- Q3/main.h
+++ Q3/main.h
@@ -48,20 +48,24 @@ typedef struct _station_
   long int packet_count;
   double accumulated_delay;
   double mean_delay;
-} Station, * Station_Ptr;
+} Station, *Station_Ptr;
 
 /**********************************************************************/
 
-typedef enum {WAITING, TRANSMITTING} Packet_Status;
+typedef enum
+{
+  WAITING,
+  TRANSMITTING
+} Packet_Status;
 
-typedef struct _packet_ 
+typedef struct _packet_
 {
   double arrive_time;
   double service_time;
   int station_id;
   Packet_Status status;
   int collision_count;
-} Packet, * Packet_Ptr;
+} Packet, *Packet_Ptr;
 
 typedef struct _simulation_run_data_
 {
@@ -74,7 +78,10 @@ typedef struct _simulation_run_data_
   long int number_of_collisions;
   double accumulated_delay;
   unsigned random_seed;
-} Simulation_Run_Data, * Simulation_Run_Data_Ptr;
+
+  double arrival_rate;
+  int number_of_stations;
+} Simulation_Run_Data, *Simulation_Run_Data_Ptr;
 
 /**********************************************************************/
 
@@ -82,15 +89,8 @@ typedef struct _simulation_run_data_
  * Function prototypes
  */
 
-int
-main(void);
+int main(void);
 
 /**********************************************************************/
 
 #endif /* main.h */
-
-
-
-
-
-
--- Q3/packet_transmission.c
+++ Q3/packet_transmission.c
@@ -133,7 +133,7 @@ transmission_end_event(Simulation_Run_Ptr simulation_run, void * packet)
     data->number_of_collisions += this_packet->collision_count;
     data->accumulated_delay += now - this_packet->arrive_time;
 
-    output_blip_to_screen(simulation_run);
+    //output_blip_to_screen(simulation_run);
 
     /* This packet is done. */
     free((void*) fifoqueue_get(buffer));
@@ -164,13 +164,13 @@ transmission_end_event(Simulation_Run_Ptr simulation_run, void * packet)
       set_channel_state(channel, IDLE);
     }
 
-    backoff_duration = 2.0*uniform_generator() * MEAN_BACKOFF_DURATION;
+    double binary_exponential_backoff = pow(2, this_packet->collision_count);
+    backoff_duration = uniform_generator() * binary_exponential_backoff;
 
     schedule_transmission_start_event(simulation_run,
 				      now + backoff_duration,
 				      (void *) this_packet);
   }
-
 }
 
 
